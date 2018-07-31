43377fd8ea246cb1446d0470634b4b7b4dd7c5d0
xiaweizi <1012126908@qq.com>
2018-07-31 13:55:33 +0800

    [dev] AsyncTask Demo 添加

---
+++ app/src/main/java/com/example/xiaweizi/threaddemo/AsyncTaskActivity.java
@@ -0,0 +1,92 @@
+package com.example.xiaweizi.threaddemo;
+
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
+
+import java.lang.ref.WeakReference;
+
+public class AsyncTaskActivity extends AppCompatActivity {
+
+    private static final String TAG = "AsyncTask::";
+    private TextView mTvContent;
+    private DownLoadFilesTask downLoadFilesTask;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_async_task);
+        mTvContent = findViewById(R.id.content);
+    }
+
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.start:
+                downLoadFilesTask = new DownLoadFilesTask(this);
+                downLoadFilesTask.execute("1234", "123456789", "1", "12", "123456789", "123", "123456");
+                break;
+            case R.id.cancel:
+                downLoadFilesTask.cancel(false);
+                break;
+        }
+    }
+
+    static class DownLoadFilesTask extends AsyncTask<String, Float, Long> {
+
+        WeakReference<AsyncTaskActivity> mActivity;
+
+        DownLoadFilesTask(AsyncTaskActivity activity) {
+            mActivity = new WeakReference<>(activity);
+        }
+
+        @Override
+        protected Long doInBackground(String... urls) {
+            Log.i(TAG, "doInBackground: " + Thread.currentThread().getName());
+            int length = urls.length;
+            long totalSize = 0;
+            for (int i = 0; i < length; i++) {
+                publishProgress(i * 1.0f / length);
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                totalSize += urls[i].length();
+            }
+            return totalSize;
+        }
+
+        @Override
+        protected void onProgressUpdate(Float... values) {
+            Log.i(TAG, "onProgressUpdate: " + values[0]);
+            updateContent(String.format("%s%%", values[0]));
+        }
+
+        @Override
+        protected void onPostExecute(Long aLong) {
+            updateContent("Complete! totalSize:\t" + aLong);
+        }
+
+        @Override
+        protected void onPreExecute() {
+            updateContent("loading....");
+        }
+
+        @Override
+        protected void onCancelled(Long aLong) {
+            super.onCancelled(aLong);
+            updateContent("已取消 " + aLong);
+        }
+
+        void updateContent(String content) {
+            AsyncTaskActivity theActivity = mActivity.get();
+            if (theActivity == null || theActivity.isFinishing()) {
+                return;
+            }
+            theActivity.mTvContent.setText(content);
+        }
+    }
+}
--- app/src/main/java/com/example/xiaweizi/threaddemo/MainActivity.java
+++ app/src/main/java/com/example/xiaweizi/threaddemo/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.xiaweizi.threaddemo;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.button:
+                startActivity(new Intent(this, AsyncTaskActivity.class));
+                break;
+            case R.id.button2:
+
+                break;
+            default:
+                break;
+        }
     }
 }
